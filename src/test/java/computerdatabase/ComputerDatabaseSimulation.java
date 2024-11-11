a88fcff30202dd4517ecb43f2a8ef17536e45390
regis066 <mugisharegis72@gmail.com>
2024-11-11 16:02:54 +0200

    Create a feeder and use it to feed virtual users

--- src/test/java/computerdatabase/ComputerDatabaseSimulation.java
+++ src/test/java/computerdatabase/ComputerDatabaseSimulation.java
@@ -1,11 +1,12 @@
 package computerdatabase;
 
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.FeederBuilder;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
-import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
-import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
 
 public class ComputerDatabaseSimulation extends Simulation {
@@ -13,11 +14,30 @@ public class ComputerDatabaseSimulation extends Simulation {
     //    Define the protocol class
     HttpProtocolBuilder httpProtocol = http.baseUrl("https://computer-database.gatling.io").acceptHeader("text/html");
 
+    //    Feeders
+    FeederBuilder.Batchable<String> feeder = csv("search.csv").random();
+
+
+    //    Business Processes
+    ChainBuilder search =
+            exec(http("Home").get("/")).pause(1).feed(feeder).exec(http("Search").get("/computers?f=#{searchCriterion" +
+                    "}").check(
+                    css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")
+            )).pause(1).exec(http(
+                    "Select").get("#{computerUrl}")).pause(1);
+
+    ChainBuilder browse = null;
+    ChainBuilder edit = null;
+    
     //    Write a scenario
-    ScenarioBuilder myScenario = scenario("My Scenario").exec(http("Request 1").get("/computers/"));
+    ScenarioBuilder users = scenario("Users").exec(search, browse);
+    ScenarioBuilder admins = scenario("Admins").exec(search, browse, edit);
+
 
     //    Define the injection profile
     {
-        setUp(myScenario.injectOpen(constantUsersPerSec(2).during(60))).protocols(httpProtocol);
+        setUp(users.injectOpen(rampUsers(10).during(10)), admins.injectOpen((rampUsers(2).during(10)))).protocols(httpProtocol);
     }
+
+
 }
